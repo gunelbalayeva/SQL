1dd71f86807d97084d5b229f54762642b79b13ff
gunelbalayeva <gunelbalayeva97@gmail.com>
2024-05-19 00:18:06 +0400

    sql

---
+++ SQL/sql_7.sql
@@ -0,0 +1,15 @@
+-- film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+select rating, count(*)
+from film
+Group by rating;
+
+-- film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı
+-- 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+
+
+-- 3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+
+
+
+-- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+--
