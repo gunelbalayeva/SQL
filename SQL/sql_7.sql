ddc7f035f282012e4eca87eb71bcdc1e0e1ce77d
gunelbalayeva <gunelbalayeva97@gmail.com>
2024-05-25 23:40:47 +0400

    sql-update

--- SQL/sql_7.sql
+++ SQL/sql_7.sql
@@ -5,11 +5,22 @@ Group by rating;
 
 -- film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı
 -- 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
-
+select replacement_cost, count(*)
+from film
+group by replacement_cost
+having count(*) > 50
 
 -- 3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+SELECT store_id, COUNT (*)
+FROM customer
+GROUP BY store_id
 
 
+-- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra
+-- en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(*) AS city
+FROM city
+GROUP BY country_id
+ORDER BY city_count DESC
+    LIMIT 1
 
--- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
---
---
+++ SQL/sql_8.sql
@@ -0,0 +1,112 @@
+
+
+-- 1.test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE,
+-- email VARCHAR(100) olan bir tablo oluşturalım.
+create table test(
+                     id integer,
+                     name varchar(50),
+                     brithday date,
+                     email varchar(100)
+);
+
+-- 2.Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+
+insert into test (id, name, brithday, email) values (1, 'Crissy', '2017-10-11', 'chandforth0@cisco.com');
+insert into test (id, name, brithday, email) values (2, 'Jilleen', null, 'jthys1@howstuffworks.com');
+insert into test (id, name, brithday, email) values (3, 'Pepi', '2009-03-12', 'ppaulley2@cisco.com');
+insert into test (id, name, brithday, email) values (4, 'Dita', '2019-06-07', 'dmacklam3@virginia.edu');
+insert into test (id, name, brithday, email) values (5, 'Rosaleen', '2007-05-22', 'rmcginty4@wikipedia.org');
+insert into test (id, name, brithday, email) values (6, 'Giorgia', null, 'gcorain5@nba.com');
+insert into test (id, name, brithday, email) values (7, 'Shauna', '2005-11-18', null);
+insert into test (id, name, brithday, email) values (8, 'Helaine', '2022-07-25', 'hstolberg7@parallels.com');
+insert into test (id, name, brithday, email) values (9, 'Maighdiln', '2013-02-24', 'mvillage8@icio.us');
+insert into test (id, name, brithday, email) values (10, 'Minette', null, null);
+insert into test (id, name, brithday, email) values (11, 'Rasia', null, 'rottama@123-reg.co.uk');
+insert into test (id, name, brithday, email) values (12, 'Iormina', '2005-03-11', 'icorreb@ibm.com');
+insert into test (id, name, brithday, email) values (13, 'Leonore', '2019-10-27', null);
+insert into test (id, name, brithday, email) values (14, 'Abbot', '2010-02-20', null);
+insert into test (id, name, brithday, email) values (15, 'Ronald', '2008-06-15', 'rbeneye@chicagotribune.com');
+insert into test (id, name, brithday, email) values (16, 'Pauli', '2005-01-12', 'pokennavainf@state.gov');
+insert into test (id, name, brithday, email) values (17, 'Ofilia', null, 'obenteg@arizona.edu');
+insert into test (id, name, brithday, email) values (18, 'Josephina', null, 'jdawidowiczh@sciencedirect.com');
+insert into test (id, name, brithday, email) values (19, 'Jeanette', '2000-10-28', 'jaxbyi@google.com.au');
+insert into test (id, name, brithday, email) values (20, 'Oliy', '2016-01-02', 'ocapronj@etsy.com');
+insert into test (id, name, brithday, email) values (21, 'Page', '2016-10-17', 'pchristink@vk.com');
+insert into test (id, name, brithday, email) values (22, 'Darcy', '2005-06-05', 'dyeal@guardian.co.uk');
+insert into test (id, name, brithday, email) values (23, 'Fleming', null, 'fosgardbym@google.fr');
+insert into test (id, name, brithday, email) values (24, 'Clair', '2017-06-07', 'cmansourn@miibeian.gov.cn');
+insert into test (id, name, brithday, email) values (25, 'Lyndy', '2016-03-19', 'ldomangeo@google.ru');
+insert into test (id, name, brithday, email) values (26, 'Lizabeth', '2002-06-09', 'lmephanp@google.com.hk');
+insert into test (id, name, brithday, email) values (27, 'Fawn', null, 'fheismanq@ning.com');
+insert into test (id, name, brithday, email) values (28, 'Correna', null, 'celderkinr@telegraph.co.uk');
+insert into test (id, name, brithday, email) values (29, 'Dominga', '2013-03-30', null);
+insert into test (id, name, brithday, email) values (30, 'Chelsea', '2016-03-31', 'cingarfillt@stumbleupon.com');
+insert into test (id, name, brithday, email) values (31, 'Bertine', '2015-02-07', 'bolinu@state.gov');
+insert into test (id, name, brithday, email) values (32, 'Odetta', '2018-11-22', null);
+insert into test (id, name, brithday, email) values (33, 'Ramonda', '2004-03-30', 'rcolbyw@deliciousdays.com');
+insert into test (id, name, brithday, email) values (34, 'Jamison', null, 'jdawex@biblegateway.com');
+insert into test (id, name, brithday, email) values (35, 'Patton', null, 'phaycrafty@miibeian.gov.cn');
+insert into test (id, name, brithday, email) values (36, 'Farah', null, 'fakisterz@paypal.com');
+insert into test (id, name, brithday, email) values (37, 'Kay', null, 'knoell10@last.fm');
+insert into test (id, name, brithday, email) values (38, 'Aretha', '2007-03-22', 'aleadbetter11@cdbaby.com');
+insert into test (id, name, brithday, email) values (39, 'Cassondra', '2014-07-25', 'cpetruk12@wordpress.org');
+insert into test (id, name, brithday, email) values (40, 'Christel', '2009-03-31', 'cjanicek13@mapy.cz');
+insert into test (id, name, brithday, email) values (41, 'Helen-elizabeth', '2004-02-23', 'hvanthoff14@ibm.com');
+insert into test (id, name, brithday, email) values (42, 'Shawn', '2023-01-01', null);
+insert into test (id, name, brithday, email) values (43, 'Yorgos', '2014-01-02', 'yahern16@theglobeandmail.com');
+insert into test (id, name, brithday, email) values (44, 'Vanya', '2015-04-27', 'varchard17@geocities.jp');
+insert into test (id, name, brithday, email) values (45, 'Hagan', null, 'hbunch18@seesaa.net');
+insert into test (id, name, brithday, email) values (46, 'Hugo', null, 'hmackean19@ucsd.edu');
+insert into test (id, name, brithday, email) values (47, 'Amii', '2014-02-03', 'aethridge1a@constantcontact.com');
+insert into test (id, name, brithday, email) values (48, 'Abbi', '2008-07-15', 'ahallums1b@cnn.com');
+insert into test (id, name, brithday, email) values (49, 'Beverlee', null, 'bstarmont1c@de.vu');
+insert into test (id, name, brithday, email) values (50, 'Cory', '2007-02-03', null);
+
+-- 3. Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+UPDATE test SET name = 'guncelleme'
+WHERE id = 5
+    RETURNING *;
+
+UPDATE test SET name = 'guncelleme 1'
+WHERE id = 7
+    RETURNING *;
+
+UPDATE test SET name = 'guncelleme 2'
+WHERE id = 10
+    RETURNING *;
+
+UPDATE test  SET name = 'guncelleme 3'
+WHERE id = 21
+    RETURNING *;
+
+UPDATE test SET name = 'guncelleme 4'
+WHERE id = 40
+    RETURNING *;
+
+UPDATE test SET name = 'guncelleme 5'
+WHERE id = 50
+    RETURNING *;
+
+-- 4. Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+delete from test
+where id = 9;
+
+delete from test
+where id = 12
+    returning *;
+
+delete from test
+where id = 16
+    returning *;
+
+
+delete from test
+where id = 40
+    returning *;
+
+
+delete from test
+where id = 36
+    returning *;
+
